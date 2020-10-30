4ac7376ad084375d043c39080abfe1a4b51fd969
Annette Odingo <odingoannette@gmail.com>
2020-10-30 21:09:47 +0300

    nthfibonacci

---
+++ nthfib/nthfibonacci.py
@@ -0,0 +1,14 @@
+"""
+The Fibonacci sequence is defined as follows: the first number of the sequence is 0, the second number is 1, and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. Write a function that takes in an integer n and returns the nth Fibonacci number.
+
+
+"""
+def getNthFib(n):
+    lastTwo = [0, 1]
+    counter = 3
+    while counter <= n:
+        nextFib = lastTwo[0] + lastTwo[1]
+        lastTwo[0] = lastTwo[1]
+        lastTwo[1] = nextFib
+        counter += 1
+    return lastTwo[1] if n > 1 else lastTwo[0]
